d6eb65fc14b7988cfc4418aab0a38a411e53bffa
Nomena <jsdev.web2@gmail.com>
2026-01-14 15:44:20 +0300

    finalisation de presque toutes les models pour les tables de l'applications

---
+++ Vue/Model_For_Table_View/Included_model.py
@@ -0,0 +1,17 @@
+from PySide6.QtGui import QStandardItemModel,QStandardItem
+from Model.SQLAlchemy.schema import Included_task_in_Price_Model
+
+
+class Adrenaline_Modeliser_For_TableView:
+    def Add_All_Data(self,equipment:list[Included_task_in_Price_Model]):
+        self.header = ["Element"]
+        self.data = [element.content for element in equipment]
+        
+    def Ready_Model(self) -> QStandardItemModel:
+        model = QStandardItemModel()
+        model.setRowCount(len(self.data))
+        model.setColumnCount(len(self.header))
+        for key,element in enumerate(self.data):
+            item = QStandardItem(element)
+            model.setItem(0,key,item)
+        return model
---
+++ Vue/Model_For_Table_View/contact_list_model.py
@@ -0,0 +1,27 @@
+from PySide6.QtGui import QStandardItemModel,QStandardItem
+from Model.SQLAlchemy.schema import Contact_Model_without_Pydantic
+
+
+class Contact_Modeliser_For_TableView():
+    def Add_All_Data(self,contact:list[Contact_Model_without_Pydantic]):
+        self.header = ["Id","Name","Mail"]
+        self.data = [[],[],[]]
+        for key,element in enumerate(contact):
+            if element.Completed:
+                self.data[0].insert(key,element.id)
+                self.data[1].insert(key,element.name)
+                self.data[2].insert(key,element.mail)
+
+    def Ready_Model(self) -> QStandardItemModel:
+        model = QStandardItemModel()
+        model.setColumnCount(len(self.header))
+        model.setRowCount(len(len(self.data[0])))
+        for index_row in range(len(self.data[0])):
+            for index_column in range(len(self.header)):
+                if index_row == 0:
+                    item = QStandardItem(self.header[index_column])
+                    model.setItem(index_row,index_column,item)
+                else:
+                    item = QStandardItem(f"{self.data[index_column][index_row - 1]}")
+                    model.setItem(index_row,index_column,item)
+        return model
